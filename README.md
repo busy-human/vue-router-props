5f51b2541ad173f02173cb52f0bcd43419e74c87
Jonathan Oaklyn <jonathan.oaklyn@busyhuman.io>
2020-02-21 13:03:19 -0700

    Properly resolve inherited props on children

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # vue-router-props
 Automatically bind to route properties and meta data and set data values on your Vue instance accordingly
+
+The RouteProps mixin makes it easy to react to route changes and updates, allowing you to specify additional data for a route on the route object itself. For example, maybe some of your routes need to be in fullscreen, or need to have the header hidden.
+
+### Installation
+
+    import {RouteProps} from "vue-router-props";
+    Vue.use(RouteProps);
+
+    // Inside your component
+    {
+        props: ["someProp"],
+        routeProps: {
+            showHeader:     { default: true, mapTo: "visible" }
+        },
+        data() {
+            return { visible: true }
+        }
+    }
+
+    // Inside your router
+    routes = [
+        { 
+            path: '/login', 
+            component: LoginView, 
+            name: "Login", 
+            meta: { showHeader: false } 
+        },
+    ]
+
+### Usage
+
+RouteProps enables you to access properties directly on the route object such as the route name, path, etc. Or, you can add custom data inside the meta field of the route object. The name you use in your routeProps field will be what is used to lookup the value in the route object.
+
+**default:** The default value will be used if there is no matching route object, or if the route does not have the field it is looking for. This allows you to cut down on redundant code and rely on the default. For example, maybe your header is usually visible in the app but on just a few views you need it to be hidden. In this case, you would only need to add the showHeader meta property to the routes that need the header hidden.
+
+**mapTo:** This will determine which field on your Vue instance the route data will be assigned to. In the example above we have a data value of visible on our header. If showHeader is true in the route object's meta field, then visible will also be set to true. mapTo is optional however, and if you don't specifiy, then RouteProps will assume the prop has the same name (e.g. showHeader)
+
+**fn:** If you need to transform the data from the route object before assigning it to your Vue field, you can pass in a function via fn. This will receive the value on the route object as the first argument. You should return the value you want assigned to the field.
+
+**requiresAuth:** The AuthGuardMiddleware allows you to protect certain routes from being accessed by unauthenticated users. If you need information about whether the current route requires authentication, you can get that information using RouteProps:
+
+    import {RouteProps} from "@busy-human/vue-component-library";
+    Vue.use(RouteProps, { router });
+
+    // Inside your component
+    {
+        props: ["someProp"],
+        routeProps: {
+            requiresAuth:     { default: false, mapTo: "authenticatedRoute" }
+        },
+        data() {
+            return { authenticatedRoute: false }
+        }
+    }
+
+In order for requiresAuth to work, you will need to make sure you pass the router into the Vue.use options for RouteProps.
---
+++ index.js
@@ -0,0 +1,103 @@
+function requireRouterOption(feature) {
+    if(!RouteProps.router) {
+        console.trace();
+        throw new Error(`Vue.use(RouteProps, {...}) should define the "router" in order to use [${feature}] on a Vue component`);
+    }
+}
+
+const RoutePropsMixin = {
+    watch: {
+        $route: function(newVal, oldVal) {
+            if(newVal !== oldVal) {
+                this.applyRouteProps();
+            }
+        }
+    },
+    // EXAMPLE
+    // routeProps: {
+    //     layout: { default: "floating", mapTo: "layout" }
+    // },
+    // END EXAMPLE
+    created() {
+        if(!RouteProps.installed) {
+            throw "RouteProps must be installed with Vue.use before using the mixin";
+        }
+    },
+    mounted() {
+        this.applyRouteProps();
+    },
+    methods: {
+        applyRouteProps() {
+            console.log("Applying route props!");
+            if(this.$options.routeProps) {
+                var keys = Object.keys(this.$options.routeProps);
+                var route = this.$route;
+                var resolvedData = resolveRouteData(route.matched);
+
+                keys.forEach(key => {
+                    var val;
+                    if(key === "requiresAuth") {
+                        requireRouterOption("RouteProps.requiresAuth");
+                        val = RouteProps.router.isAuthenticatedRoute(route);
+                    } else {
+                        val = resolvedData[key] || resolvedData.meta[key];
+                    }
+                    var option = this.$options.routeProps[key];
+
+                    var mapTo = option.mapTo || key;
+                    var mapFn = option.fn;
+
+                    // If no routes were matched or the route did not specify a value then use the default
+                    if(route.matched.length === 0 || val === undefined) {
+                        this[mapTo] = option.default;
+                    } else if(mapFn) {
+                        this[mapTo] = mapFn(val);
+                    } else {
+                        this[mapTo] = val;
+                    }                    
+                });
+            }
+        }
+    }
+}
+
+/**
+ * Applies inheritance as necessary
+ * @param {*} matchedRoutes 
+ */
+function resolveRouteData(matchedRoutes) {
+    var resolvedObj = { meta: {} };
+    if(!matchedRoutes || matchedRoutes.length === 0) {
+        return resolvedObj;
+    } else if(matchedRoutes.length == 1) {
+        return matchedRoutes[0];
+    } else {
+        for(let i = 0; i < matchedRoutes.length; i++) {
+            let r = matchedRoutes[i];
+            var meta = Object.assign(resolvedObj.meta, r.meta || {});
+            resolvedObj = Object.assign(resolvedObj, r || {});
+            resolvedObj.meta = meta;
+        }
+        return resolvedObj;
+    }
+}
+
+export const RouteProps = {
+    mixin:      RoutePropsMixin,
+    router:     null,
+    installed:  false,
+    install:    function(Vue, { global = true, router } = {global: false}) {
+        var strategies = Vue.config.optionMergeStrategies;
+        strategies.routeProps = strategies.methods;
+        RouteProps.installed = true;
+
+        if(global) {
+            Vue.mixin(RoutePropsMixin);
+        }
+        if(router) {
+            RouteProps.router = router;
+        }
+    }
+};
+
+export default RouteProps;
